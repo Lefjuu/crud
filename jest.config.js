881be0288d8b434c34bfab48a269ca79577c00c4
Lefjuu <karol.legut121@gmail.com>
2026-01-14 22:12:48 +0100

    chore: add testing scripts and dependencies for Jest and Testing Library

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  testEnvironment: "jsdom",
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/**/*.d.ts",
+    "!src/generated/**",
+  ],
+};
+
+module.exports = createJestConfig(customJestConfig);
